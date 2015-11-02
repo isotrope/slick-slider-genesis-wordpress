c96d7d006cf67177b0b89f97d55d446afaf46d7e
sstruemph <steve@stevestruemph.com>
2015-11-02 13:35:39 -0600

    General cleanup of code. One edit in Fancybox CSS

--- slick-slider-genesis-wordpress/js/fancybox-init.js
+++ slick-slider-genesis-wordpress/js/fancybox-init.js
@@ -1,6 +1,4 @@
 jQuery(function( $ ){
-
-	/* This is basic - uses default settings */
 	
 	$('a.fancybox-ss').fancybox({
 		'transitionIn'	:	'fade',
@@ -13,29 +11,6 @@ jQuery(function( $ ){
 		'padding'		:	0,
 		'cyclic'		:	true,
 		'centerOnScroll':	true
-	});
-	
-	$(".iframe").fancybox({
-		'width'         : '75%',
-		'height'        : '85%',
-		'autoScale'     : false,
-		'transitionIn'  : 'none',
-		'transitionOut' : 'none',
-		'type'          : 'iframe',
-		'centerOnScroll': true
-	});
-	
-	$("a.iframeCommentForms").fancybox({
-		'width'		      :	600,
-		'height'		      :	650,
-		'overlayOpacity'	 :	'0.4',
-		'overlayColor'		 :	'#000',
-		'hideOnContentClick' :   false,
-		'autoScale'     	 :   false,
-   		'transitionIn'		 :   'elastic',
-		'transitionOut'      :   'elastic',
-		'wrapCSS'            :   'comment-form-lightbox-class',
-		'type'			     :   'iframe'
 	});
 	
 });
